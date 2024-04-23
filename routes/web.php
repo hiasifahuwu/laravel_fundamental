fa4b2c15396fd975a5891107561c70caf7f8ab07
SifahuwuHia <hia.sifahuwu@gmail.com>
2024-04-24 02:32:22 +0700

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/list-menu/get-all-data', [MenuController::class, 'getAllData']);
+
+Route::get('/list-menu', function () {
+    return "Ini adalah halaman yang menampilkan semua menu di Cafe Amandemy..";
+});
+
+Route::get('/list-menu/{namaMenu}/{harga}', [MenuController::class, 'getAllData']);
